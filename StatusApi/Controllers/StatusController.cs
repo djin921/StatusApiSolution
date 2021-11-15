ce829c60ec97e6ef922d862e758be8f397c0fd4a
Daniel Jin <danieljin921@gmail.com>
2021-11-15 11:09:54 -0500

    Add project files.

---
+++ StatusApi/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StatusApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    // GET /status
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        var response = new StatusResponse
+        {
+            Message = "The Server is Great.. Thanks",
+            LastChecked = DateTime.Now
+        };
+        return Ok(response);
+    }
+}
+
+
+public class StatusResponse
+{
+    public string Message { get; set; }
+    public DateTime LastChecked { get; set; }
+}
